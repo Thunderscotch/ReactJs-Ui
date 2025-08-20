d80f30a821c51009cda8e9130503f1a3728fd0ea
Thunderscotch <subashnatrayan28@gmail.com>
2025-08-20 22:56:21 +0530

    styled package installed and used

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,33 @@ import { useState, useEffect } from "react";
 import darkImg from "./assets/imagedark.jpg";
 import lightImg from "./assets/imagelight.jpg";
 import { Card } from './components/Card/card';
+import styled from "styled-components";
+
+const Bubble = styled.div`
+  position: absolute;
+  width: 30%;
+  height: 20%;
+  border-radius: 120px;
+  background-color: ${props => props.color? "rgba(8, 104, 104, 0.425)" : "rgba(242, 235, 193, 0.959)"};
+  color: ${(props) => (props.color ? "var(--green-blue)" : "var(--copper-rose)")};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  top: 50%;
+  left: -90px;
+  transition: all 0.3s ease;
+
+  &:hover {
+    transform: scale(1.05) translate(5px, -5px);
+    background-color: ${props => props.color? " var(--green-blue)" : "var(--mint-julip)"};
+    color: ${props => props.color? "var(--bunker-dark)" : "var(--copper-rose)"};
+  }
+`;
+
+const Title = styled.h2`{
+  line-height: 1.1;
+  transition: all 0.3s ease;
+}`
 
 function App() {
   const [isDark, setisDark] = useState(true);
@@ -33,6 +60,10 @@ function App() {
           The Memories: <br />
           That Fades Over Time
         </h1>
+
+      <Bubble color={isDark}>
+          <Title> {isDark ? "Life Becomes dark sometimes" : "But Light Shines In the Dark"} </Title>
+      </Bubble>
       
       <Card 
       title="Illustration"

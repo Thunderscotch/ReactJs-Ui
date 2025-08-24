a9394f87afe143edd2ced0437a024e1807761168
Thunderscotch <subashnatrayan28@gmail.com>
2025-08-24 13:48:49 +0530

    Nav Update

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,29 +1,53 @@
 import styled from "styled-components";
-import cn from "clsx";
+import cn from "classnames";
 
-const nav = styled.nav`{
+const NavStyle = styled.nav`
     display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-    text-align: center;
-    width: 77%;
+    justify-content: center;
+    justify-self: center;
+    width: 50%;
     top: 20px;
     border-radius: 20px;
     padding: 0.5rem 1rem;
-    background-color: ${isDark ? "rgba(8, 104, 104, 0.425)" : "var(--copper-rose)"};
-    color: ${isDark ? "var(--green-blue)" : "var(--mint-julip)"};
+    background-color: ${(props) => props.isDark ? "var(--green-blue)" : "var(--copper-rose)"};
     transition: all 0.3s ease;
     -webkit-backdrop-filter: blur(5px);
     backdrop-filter: blur(5px);
-    z-index: 2;
+    z-index: 3;
     position: sticky;
-    margin: -800px auto 10px;
-}`
+
+    ul {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        list-style: none;
+        margin: 0;
+        padding: 0;
+    }
+
+    li {
+        margin: 0 10px;
+    }
+
+    a { 
+        padding: 0.5rem 1rem;
+        text-decoration: none;
+        color: ${(props) => props.isDark ? "var(--bunker-dark)" : "var(--mint-julip)"};
+        border-radius: 50px;
+        transition: all 0.3s ease;
+    }
+
+    a:hover {
+        color: ${(props) => props.isDark ? "var(--green-blue)" : "var(--copper-rose)"};
+        background-color: ${(props) => props.isDark ? "var(--bunker-dark)" : "var(--copper-rose)"};
+        border-radius: 50px;
+    }
+    `;
 
 export const Nav = ({ navigators, ...props }) => {
     return (
-        <nav {...props} className={cn("nav", props.className)}>
+        <NavStyle {...props} isDark={props.isDark} className={cn(props.className)}>
             <ul>
                 {navigators.map((navigator) => (
                     <li key={navigator.id}>
@@ -31,7 +55,7 @@ export const Nav = ({ navigators, ...props }) => {
                     </li>
                 ))}
             </ul>
-        </nav>
+        </NavStyle>
     )
 }
 
